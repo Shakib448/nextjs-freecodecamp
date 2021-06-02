e1ec50bb6f03ab5fbbf725aafa3652e0e9a3a31f
Shakiba448 <shakiba448@gmail.com>
2021-06-02 09:32:55 +0600

    WIP on senity

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -4,6 +4,8 @@ import createSchema from "part:@sanity/base/schema-creator";
 // Then import schema types from any plugins that might expose them
 import schemaTypes from "all:part:@sanity/base/schema-type";
 import chef from "./chef";
+import ingredient from "./ingredient";
+import recipe from "./recipe";
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
   // We name our schema
@@ -13,5 +15,7 @@ export default createSchema({
   types: schemaTypes.concat([
     /* Your types here! */
     chef,
+    ingredient,
+    recipe,
   ]),
 });
