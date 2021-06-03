1433a2fc24b60c155b17ce0c9b7093bd1261226b
Shakiba448 <shakiba448@gmail.com>
2021-06-03 11:15:33 +0600

    WIP on senity

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,25 @@ const recipesQuery = `*[_type == "recipe"]{
 
 export default function Home({ recipes }) {
   console.log(recipes);
-  return <Layout></Layout>;
+  return (
+    <div>
+      <h1>Welcome to Kap's Kitchen 🍍</h1>
+
+      <ul className="recipes-list">
+        {recipes?.length > 0 &&
+          recipes.map((recipe) => (
+            <li key={recipe._id} className="recipe-card">
+              <Link href={`/recipes/${recipe.slug.current}`}>
+                <a>
+                  <img src={urlFor(recipe.mainImage).url()} alt={recipe.name} />
+                  <span>{recipe.name}</span>
+                </a>
+              </Link>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
 }
 
 export const getStaticProps = async () => {
