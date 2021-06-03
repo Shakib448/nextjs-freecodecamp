218c7667ad989f03228aac792674166ddcc0b298
Shakiba448 <shakiba448@gmail.com>
2021-06-03 11:56:46 +0600

    WIP on senity

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -5,6 +5,24 @@ import {
   PortableText,
 } from "../../lib/sanity";
 
+const recipeQuery = `*[_type == "recipe" && slug.current == $slug][0]{
+    _id,
+    name,
+    slug,
+    mainImage,
+    ingredient[]{
+      _key,
+      unit,
+      wholeNumber,
+      fraction,
+      ingredient->{
+        name
+      }
+    },
+    instructions,
+    likes
+  }`;
+
 const OneRecipe = () => {
   return (
     <div>
@@ -14,3 +32,24 @@ const OneRecipe = () => {
 };
 
 export default OneRecipe;
+
+export const getStaticPaths = async () => {
+  const paths = await sanityClient.fetch(
+    `*[_type == "recipe" && defined(slug.current)]{
+          "params": {
+            "slug": slug.current
+          }
+        }`
+  );
+
+  return {
+    paths,
+    fallback: true,
+  };
+};
+
+export const getStaticProps = async ({ params }) => {
+  const { slug } = params;
+  const recipe = await sanityClient.fetch(recipeQuery, { slug });
+  return { props: { data: { recipe }, preview: true } };
+};
