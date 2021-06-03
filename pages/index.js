0c76567fbd54eb5b4a0ba14412042fbe1443034e
Shakiba448 <shakiba448@gmail.com>
2021-06-03 10:24:23 +0600

    WIP on senity

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import Link from "next/link";
 import Layout from "../components/Layout";
+import { sanityClient, urlFor } from "../lib/sanity";
+
+const recipesQuery = `*[_type == "recipe"]`;
 
 export default function Home() {
   return <Layout></Layout>;
